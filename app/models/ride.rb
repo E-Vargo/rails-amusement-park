f8f9e6fe21bf153666433656f51414f2a59df279
E-Vargo <vargoelliott@gmail.com>
2022-03-17 02:03:00 -0700

    commit tree 0209f3ced292f3fcdcb45b2d7aa58bec3acf5573

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,32 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user 
+    belongs_to :attraction
+
+    def take_ride
+        if eligible ==  "Thanks for riding the #{attraction.name}!"
+            alter_all
+        else 
+            eligible
+        end
+    end
+
+    def eligible 
+        if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height 
+            "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+        elsif self.user.height < self.attraction.min_height 
+             "Sorry. You are not tall enough to ride the #{attraction.name}."
+        elsif  self.user.tickets < self.attraction.tickets
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        else
+            "Thanks for riding the #{attraction.name}!"
+        end
+    end
+
+    def alter_all
+    self.user.update(
+        happiness: self.user.happiness += self.attraction.happiness_rating,
+        nausea: self.user.nausea += self.attraction.nausea_rating,
+        tickets: self.user.tickets -= self.attraction.tickets
+        )
+    end
 end
